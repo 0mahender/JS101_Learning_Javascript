ede7e3fbfd50ba3059ad7d99605a01a7a15e7a84
0mahender <115463358+0mahender@users.noreply.github.com>
2022-10-15 15:39:23 +0530

    Delete day1 prgm directory

--- day1 prgm/prablem1.js
+++
@@ -1,7 +0,0 @@
-let a = 3;
-let b = 2;
-
-console.log("sum is",a+b);
-console.log("sub is",a-b);
-console.log("muli is",a*b);
-
--- day1 prgm/prablem2.js
+++
@@ -1,3 +0,0 @@
-let x=16;
-console.log(x**0.5);
-console.log("mahi");
